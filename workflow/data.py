95152f9abd1a24d17fdac24f44e5f6aff48f53e0
xpureheroinex <daryashapovalova7@gmail.com>
2019-04-23 17:36:56 +0300

    create package to download data

---
+++ workflow/data.py
@@ -0,0 +1,9 @@
+import os
+URL = "https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD"
+def get_data(filename='Fremont.csv', url=URL, force_download=False):
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data.columns = ['East', 'West']
+    data['Total'] = data['East'] + data['West']
+    return data
