c4d786a9e23eccbce23b99da4140b4213cc8a245
xpureheroinex <daryashapovalova7@gmail.com>
2019-04-23 18:08:24 +0300

    add PCA, compare with days of week

--- workflow/data.py
+++ workflow/data.py
@@ -5,7 +5,11 @@ URL = "https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD
 def get_data(filename='Fremont.csv', url=URL, force_download=False):
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
     data.columns = ['East', 'West']
     data['Total'] = data['East'] + data['West']
     return data
